996be57015c74d4352a1e5db7512a5c833aef7ad
nurami <mrnursil@gmail.com>
2018-04-24 20:14:14 +0300

    show my courses on dashboard

--- app/controllers/learners/dashboard_controller.rb
+++ app/controllers/learners/dashboard_controller.rb
@@ -1,5 +1,9 @@
 module Learners
   class DashboardController < BaseController
+
+    expose :course, find_by: :slug, id: :slug
+    expose :courses, from: :current_learner
+    
     def show; end
   end
 end
